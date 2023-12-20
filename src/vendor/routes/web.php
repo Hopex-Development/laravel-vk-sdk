9d03cbc49b40aa6cfd8447f659ae091eaa729284
Schizo <hopex.schizo@gmail.com>
2023-12-20 03:44:54 +0300

    Обновление модели пользователя

--- src/vendor/routes/web.php
+++ src/vendor/routes/web.php
@@ -1,12 +1,12 @@
 <?php
 
 use GuzzleHttp\Client;
-use Hopex\VkSdk\Models\Application;
-use Hopex\VkSdk\Models\Users;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
 use Hopex\VkSdk\Foundation\Core\Configuration\Config;
+use Hopex\VkSdk\Models\Application;
+use Hopex\VkSdk\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,36 +19,39 @@ use Hopex\VkSdk\Foundation\Core\Configuration\Config;
 |
 */
 
-Route::get('auth/callback', function (\Illuminate\Http\Request $request) {
+Route::get('auth/callback', function (Request $request) {
     $payload = json_decode($request->get('payload'), true);
 
-    Users::updateOrCreate([
+    User::updateOrCreate([
         'user_id' => data_get($payload, 'user.id'),
     ], array_merge($payload, data_get($payload, 'user')));
 
     $response = Http::setClient(new Client(['verify' => false]))
         ->get('https://api.vk.com/method/auth.exchangeSilentAuthToken', [
-            'v' => (new Config())->api()->getVersion(),
+            'v' => (new Config())->api()->version(),
             'token' => data_get($payload, 'token'),
             'access_token' => Application::first()->service_access_key,
             'uuid' => data_get($payload, 'uuid'),
         ])->body();
     $response = data_get(json_decode($response, true), 'response');
 
-    Users::updateOrCreate([
-        'user_id' => data_get($response, 'user_id'),
-    ], collect($response)->only([
-        'phone',
-        'phone_validated',
-        'additional_signup_required',
-        'is_service',
-        'email',
-        'source',
-        'source_description',
-        'access_token',
-        'access_token_id',
-        'expires_in',
-    ])->toArray());
+    User::updateOrCreate(
+        [
+            'user_id' => data_get($response, 'user_id'),
+        ],
+        collect($response)->only([
+            'phone',
+            'phone_validated',
+            'additional_signup_required',
+            'is_service',
+            'email',
+            'source',
+            'source_description',
+            'access_token',
+            'access_token_id',
+            'expires_in',
+        ])->toArray()
+    );
 
     return redirect()->route('login');
 });
